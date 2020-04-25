a166d910bbe782ec300b6fe31d8783bbaf710dc5
Andrey Azov <andrey.azov@gmail.com>
2020-04-25 17:44:29 +0100

    Debugging diagnostics function

--- api/diagnostics.js
+++ api/diagnostics.js
@@ -2,10 +2,12 @@ const config = require('../config');
 
 const runDiagnostics = async (req, res) => {
   const databasePath = process.env.DEPLOYMENT === 'NOW' ? 'build/database.db' : config.databasePath;
+  console.log('PROCESS ENV': process.env);
   const diagnosticsData = {
     deploymentEnv: process.env.DEPLOYMENT,
     databasePath
-  }
+  };
+  console.log('diagnosticsData', diagnosticsData);
 
   res.json(diagnosticsData);
 };
